6bdb53f65bbe197045a5a16b50c8aa41b3ad181f
ardicDemirol <ardc.dmrol@gmail.com>
2024-08-01 22:50:22 +0300

    docker file added

--- GameStore/obj/Release/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Release/net8.0/GameStore.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("b8757410-f961-4882-97f2-5b9860fc8a7c")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add01db1fd73ea7d03c9e8e3f4249456d0a08232")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
